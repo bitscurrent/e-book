62a91bf49ce5ec6972775821b08973fc5885785f
Dilip <dilipboidya.office@gmail.com>
2024-05-01 23:33:02 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,10 @@
-console.log("Hii there")
+import app from "./src/app";
+
+const startServer = () => {
+    const PORT = process.env.PORT || 4600;
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT}`)
+    })
+}
+
+startServer()
