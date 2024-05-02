3229436c406ba831cab90ced64ea535013e62e6c
Dilip <dilipboidya.office@gmail.com>
2024-05-02 16:47:25 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import express from "express"
+import express,{Request, Response,NextFunction} from "express"
+import createHttpError, { HttpError } from "http-errors";
+import { config } from "./config/config";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express()
 
 app.get("/", (req, res) => {
+    
+    const error = createHttpError(400, "Kuch galat hoi gaawa")
+    throw error
     res.json({message: "Welcome"})
 })
 
-app.use("/", (req, res,) => {
-    res.json({})
-})
+// global error handler
+app.use(globalErrorHandler)
+
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,8 @@ conf()
 const _config = {
     port: process.env.PORT,
     mongodb_uri: process.env.MONGODB_URI, 
+
+    env: process.env.NODE_ENV,
 }
 
 export const config = Object.freeze(_config) //becomes readonly
