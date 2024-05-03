c721611bb0a1abea3b06b9eba5545579f8173249
Dilip <dilipboidya.office@gmail.com>
2024-05-03 23:27:20 +0530

    create user router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express,{Request, Response,NextFunction} from "express"
 import createHttpError, { HttpError } from "http-errors";
 import { config } from "./config/config";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express()
 
@@ -11,6 +12,8 @@ app.get("/", (req, res) => {
     throw error
     res.json({message: "Welcome"})
 })
+
+app.use("/api/users",userRouter)
 
 // global error handler
 app.use(globalErrorHandler)
