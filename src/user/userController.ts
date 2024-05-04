98c37035b165ede459177966168ca8de99eb6be7
Dilip <dilipboidya.office@gmail.com>
2024-05-05 00:02:23 +0530

    add login

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -51,7 +51,32 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
 
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are mandatory"));
+  }
+  // try catch
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return next(createHttpError(404, "User not found."));
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return next(createHttpError(400, "Username or password inncorrect."));
+  }
+
+  // token
+
+  const token = sign({ sub: user._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
+
+  res.json({ accessToken: token });
 };
 
-export { createUser };
+export { createUser, loginUser };
