0919cd76729b70c1f75aad46d4b900c86c475c43
Dilip <dilipboidya.office@gmail.com>
2024-05-08 14:11:25 +0530

    add app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,23 @@
-import express,{Request, Response,NextFunction} from "express"
+import express, { Request, Response, NextFunction } from "express";
 import createHttpError, { HttpError } from "http-errors";
 import { config } from "./config/config";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 
-const app = express()
+const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    
-    const error = createHttpError(400, "Kuch galat hoi gaawa")
-    throw error
-    res.json({message: "Welcome"})
-})
+  const error = createHttpError(400, "Kuch galat hoi gaawa");
+  throw error;
+  res.json({ message: "Welcome" });
+});
 
-app.use("/api/users",userRouter)
+app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 
 // global error handler
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 export default app;
